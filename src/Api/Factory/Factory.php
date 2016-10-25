db6b868a53f07764fd785b8e6f1b56d8481558f2
Benjamin Heller <ben-hx@web.de>
2016-10-25 21:01:40 +0200

    test

--- src/Api/Factory/Factory.php
+++ src/Api/Factory/Factory.php
@@ -95,6 +95,7 @@ class Factory
         };
         $container['UserRepository'] = function ($container) {
             //return new UserFileRepository($this->getFileInfoForFileName($this->dbBaseDirecotry.$this->userRepositoryFileName));
+            throw new \Exception(getcwd());
             return new UserFileRepository(new \SplFileInfo('./db/evelopment/UserRepository'));
         };
         $container['errorHandler'] = function ($container) {
